c99d95eb9a79b823fc46af0937f6da125798b120
KAN <ogawakan@icloud.com>
2025-11-07 19:58:59 +0900

    メイン画面作成,完了リスト画面ト最初だけ

---
+++ src/pages/delete.jsx
@@ -0,0 +1,13 @@
+export default function Del(){
+
+return (
+    <div>
+        <div className ='top'> 完了したリスト</div>
+        <div className="left"></div>
+
+        <div className="center"></div>
+
+        <div className="right"></div>
+    </div>
+);
+}
